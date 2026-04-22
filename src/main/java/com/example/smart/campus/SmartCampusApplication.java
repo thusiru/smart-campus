4399d36ade561fae6c69be8d326be6ce4e0bf466
Thusiru Kodithuwakku <147712158+thusiru@users.noreply.github.com>
2026-04-22 17:11:14 +0530

    Create application start point

---
+++ src/main/java/com/example/smart/campus/SmartCampusApplication.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smart.campus;
+
+/**
+ *
+ * @author Thusiru Kodithuwakku
+ */
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("/api/v1")
+public class SmartCampusApplication extends Application {
+
+}
