c7cc1f5b9c016e7fe14692469e60b0de858bfa47
Thusiru Kodithuwakku <147712158+thusiru@users.noreply.github.com>
2026-04-23 00:15:24 +0530

    Create DataStore to act as in-memory database

---
+++ src/main/java/com/example/smart/campus/data/DataStore.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smart.campus.data;
+
+import com.example.smart.campus.models.Room;
+import com.example.smart.campus.models.Sensor;
+import com.example.smart.campus.models.SensorReading;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ *
+ * @author Thusiru Kodithuwakku
+ */
+public class DataStore {
+
+    public static final GenericDAO<Room> roomDAO = new GenericDAO<>(new CopyOnWriteArrayList<>());
+    public static final GenericDAO<Sensor> sensorDAO = new GenericDAO<>(new CopyOnWriteArrayList<>());
+    public static final GenericDAO<SensorReading> radingDAO = new GenericDAO<>(new CopyOnWriteArrayList<>());
+
+    private DataStore() {
+    }
+}
