ee9ffd3b3f259d1f508c5e6fc738d76d1484b9a4
Thusiru Kodithuwakku <147712158+thusiru@users.noreply.github.com>
2026-04-22 18:10:22 +0530

    Create base model interface

---
+++ src/main/java/com/example/smart/campus/models/BaseModel.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smart.campus.models;
+
+/**
+ *
+ * @author Thusiru Kodithuwakku
+ */
+public interface BaseModel {
+
+    String getId();
+
+    void setId(String id);
+}
