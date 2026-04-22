37ebfed6a3109fbd1e183742fa11d8282e056579
Thusiru Kodithuwakku <147712158+thusiru@users.noreply.github.com>
2026-04-22 18:38:08 +0530

    Create SensorReading POJO model

---
+++ src/main/java/com/example/smart/campus/models/SensorReading.java
@@ -0,0 +1,51 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smart.campus.models;
+
+/**
+ *
+ * @author Thusiru Kodithuwakku
+ */
+public class SensorReading implements BaseModel {
+
+    private String id;
+    private long timestamp;
+    private double value;
+
+    public SensorReading() {
+    }
+
+    public SensorReading(String id, long timestamp, double value) {
+        this.id = id;
+        this.timestamp = timestamp;
+        this.value = value;
+    }
+
+    @Override
+    public String getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+}
