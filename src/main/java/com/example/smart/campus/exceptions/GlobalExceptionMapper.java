679fee04dfc78804dc67c6591d6a0d81ae850cbf
Thusiru Kodithuwakku <147712158+thusiru@users.noreply.github.com>
2026-04-23 15:10:33 +0530

    Implement GlobalExceptionMapper

---
+++ src/main/java/com/example/smart/campus/exceptions/GlobalExceptionMapper.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.example.smart.campus.exceptions;
+
+import com.example.smart.campus.models.ErrorMessage;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+import javax.ws.rs.ext.Provider;
+
+/**
+ *
+ * @author Thusiru Kodithuwakku
+ */
+@Provider
+public class GlobalExceptionMapper implements ExceptionMapper<Throwable> {
+
+    @Override
+    public Response toResponse(Throwable exception) {
+        ErrorMessage error = new ErrorMessage(
+                "An unexpected internal server error occurred. Please contact support.",
+                500,
+                "https://api.smartcampus.com/docs/errors/internal-error"
+        );
+
+        return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(error).type(MediaType.APPLICATION_JSON).build();
+    }
+}
